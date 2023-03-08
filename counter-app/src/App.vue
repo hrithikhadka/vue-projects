e6b8f7f726b50977e5b10ad1fb077599a50ab4ab
Hrithik Khadka <hritik.khadka@centria.fi>
2023-03-08 13:50:20 +0200

    add css

--- counter-app/src/App.vue
+++ counter-app/src/App.vue
@@ -11,4 +11,34 @@
   </main>
 </template>
 
-<style></style>
+<style scoped>
+main {
+  background-color: aliceblue;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+main div {
+  text-align: center;
+}
+
+h1 {
+  font-size: 4rem;
+}
+
+h3 {
+  font-size: 3rem;
+}
+
+button {
+  font-size: 1.5rem;
+  padding: 0.5rem;
+  width: 20%;
+  margin-right: 1rem;
+  margin-left: 1rem;
+  cursor: pointer;
+}
+</style>
