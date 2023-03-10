1ef277c82e9c1910b61900b62b69c19daaeb3301
Hrithik Khadka <hritik.khadka@centria.fi>
2023-03-10 12:27:48 +0200

    toggle modal

--- modal/src/App.vue
+++ modal/src/App.vue
@@ -1,8 +1,33 @@
-<script setup></script>
-
 <template>
-  <h1>hello!</h1>
+  <h1>{{ title }}</h1>
+  <p>Toggling Modal</p>
+  <div v-if="showModal">
+    <Modal :header="header" :text="text" theme="sale" @close="toggleModal" />
+  </div>
+  <button @click="toggleModal">Open Modal</button>
 </template>
+
+<script>
+import Modal from "./components/Modal.vue";
+
+export default {
+  name: "App",
+  components: { Modal },
+  data() {
+    return {
+      title: "Vue App!",
+      header: "Sign up for Giveaway!",
+      text: "Grab boots for half the price!",
+      showModal: false,
+    };
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+  },
+};
+</script>
 
 <style scoped>
 header {
---
+++ modal/src/components/Modal.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="back" @click="closeModal">
+    <div class="modal" :class="{ sale: theme === 'sale' }">
+      <h1>{{ header }}</h1>
+      <p>{{ text }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["header", "text", "theme"],
+  methods: {
+    closeModal() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
